52dd1915da8a9677124d1078aeafbb64d8596705
mcosic96 <62704400+mcosic96@users.noreply.github.com>
2020-03-26 15:15:23 +0100

    kolaboratorova promjena
    dodan komentar od strane kolaboratora

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //govno
+            //komentar kolaboratora Mate Cosica
             Console.WriteLine("prva promjena koda");
         }
     }
